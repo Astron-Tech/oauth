9dca51b68dcce3d8b443c97b0942e4dd207f11f1
Astron <dylanjtobin2014@gmail.com>
2025-08-28 07:55:38 -0400

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,9 +1,12 @@
-const { Pool } = require('pg');
-const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+const mariadb = require('mariadb');
+
+const pool = mariadb.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
+  connectionLimit: 5
 });
+
 module.exports = { pool };
